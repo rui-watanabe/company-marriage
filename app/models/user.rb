86e2d6f0d0f05634f6b575ec682eab5a8182e14d
rui-watanabe <wataru610806@gmail.com>
2019-08-18 20:40:45 +0900

    validation_devise
    # What
    Userモデルに対してのバリデーションを組む
    
    # Why
    セキュリティーの面から不正なユーザーからの新規登録を防ぐため

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :profile, presence: true
+      
 end
