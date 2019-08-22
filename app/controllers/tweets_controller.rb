bc1fa1cc1924005930ef891c455f22769cbb3795
rui-watanabe <wataru610806@gmail.com>
2019-08-22 20:28:14 +0900

    create_controller_tweet

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,25 @@
+class TweetsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+  end
+
+  def new
+  end
+
+  def create
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    flash[:notice] = "投稿しました"
+    redirect_to root_path
+  end
+  
+  private
+  def tweet_params
+    params.permit(:text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+end
