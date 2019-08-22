555a961ad1a062568ea6f189f01c7a25977f9952
rui-watanabe <wataru610806@gmail.com>
2019-08-22 21:38:47 +0900

    create_tweet_destroy

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -32,6 +32,11 @@ class TweetsController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
+  end
   
   private
   def tweet_params
