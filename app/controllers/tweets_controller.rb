06fb34ddf828a42fa10f20eb79bbbb6bd8920c83
rui-watanabe <wataru610806@gmail.com>
2019-08-22 20:49:05 +0900

    create_tweets-controller_show

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,11 @@ class TweetsController < ApplicationController
     flash[:notice] = "投稿しました"
     redirect_to root_path
   end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    @comments = @tweet.comments.includes(:user)
+  end
   
   private
   def tweet_params
