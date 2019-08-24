8fd42c30ee7a8f625edc011d0b3ace012c403bb6
rui-watanabe <wataru610806@gmail.com>
2019-08-24 18:53:44 +0900

    create_users-controller_edit,update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,17 @@ class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
   end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
   
 end
