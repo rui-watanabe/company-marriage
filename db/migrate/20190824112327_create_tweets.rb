0556272b2d0d1975c9e368bf8ca96b323dd2ff19
rui-watanabe <wataru610806@gmail.com>
2019-08-24 20:34:25 +0900

    create_comments-table
    # What
    コメント機能のテーブル作成
    
    # Why
    どのユーザーがどの投稿に関してコメントしたか管理するため

--- db/migrate/20190824112327_create_tweets.rb
+++ db/migrate/20190824112327_create_tweets.rb
@@ -2,7 +2,6 @@ class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
       t.text        :text
-      t.text        :image
       t.integer     :user_id
       t.timestamps null: true
     end
