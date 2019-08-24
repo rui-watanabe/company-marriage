d63cd6f60eefce013f35d7144a6d495de073c2df
rui-watanabe <wataru610806@gmail.com>
2019-08-24 17:53:49 +0900

    create_users-controller_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-
+  
+  before_action :authenticate_user!, only: [:show]
+  
   def index
     @users = User.where.not(id: current_user.id, status: 0).page(params[:page]).per(5).order("created_at DESC")
   end
-  
+
+  def show
+    @user=User.find(params[:id])
+  end
+
 end
