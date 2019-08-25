02df2c4c8b03805945a2320313db9ed42b383d4f
rui-watanabe <wataru610806@gmail.com>
2019-08-25 11:57:44 +0900

    create_messages-controller_show

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,18 @@
+class MessagesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:create]
+  
+  def create
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+      @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
+      respond_to do |format|
+        format.html { redirect_to "/rooms/#{@message.room_id}"}
+        format.json
+    end
+    else
+      @messages = messages.includes(:user)
+      flash.now[:alert] = "メッセージを入力してください。"
+      render template: "room/new"
+    end
+  end
+end
