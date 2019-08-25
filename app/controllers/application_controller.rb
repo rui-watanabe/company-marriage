22472355fecbea01c8a9f8152010b34b08fac3c2
rui-watanabe <wataru610806@gmail.com>
2019-08-25 12:59:25 +0900

    update_application-controller_permitted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  add_flash_types :success, :info, :warning, :danger
+
+  def after_sign_out_path_for(resource)
+    home_path 
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :profile])
+  end
+  
 end
