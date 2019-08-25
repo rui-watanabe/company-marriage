1c4be7f8b5818dcc1bf31a0cfc11f2ba137f31c3
rui-watanabe <wataru610806@gmail.com>
2019-08-25 11:54:38 +0900

    create_rooms-controller_show

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,5 +7,14 @@ class RoomsController < ApplicationController
     redirect_to "/rooms/#{@room.id}"
   end
 
-  
+  def show
+    @room = Room.find(params[:id])
+    if Entry.where(:user_id => current_user.id, :room_id => @room.id).present?
+      @messages = @room.messages
+      @message = Message.new
+      @entries = @room.entries
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
