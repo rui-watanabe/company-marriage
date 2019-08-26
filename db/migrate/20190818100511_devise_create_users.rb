463062cc20706c1344302a1688f5bbe0032e39be
rui-watanabe <wataru610806@gmail.com>
2019-08-26 09:34:44 +0900

    Update 20190818100511_devise_create_users.rb

--- db/migrate/20190818100511_devise_create_users.rb
+++ db/migrate/20190818100511_devise_create_users.rb
@@ -1,31 +1,48 @@
-<div class="contentns" style="width:50%; margin:0 auto;">
-<h1 style="padding-top:30px;"><%= t('ログイン') %></h1>
+# frozen_string_literal: true
 
-<%= form_for(resource, as: resource_name, url: session_path(resource_name)) do |f| %>
-  <div class="form-group">
-    <%= f.label :email %>
-    <%= f.email_field :email, autofocus: true, autocomplete: 'email', class: 'form-control' ,placeholder:"test@gmail.com"%>
-  </div>
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
 
-  <div class="form-group">
-    <%= f.label :password %>
-    <%= f.password_field :password, autocomplete: 'current-password', class: 'form-control' ,placeholder:"パスワードを入力してください"%>
-  </div>
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
-  <% if devise_mapping.rememberable? %>
-    <div class="form-group form-check">
-      <%= f.check_box :remember_me, class: 'form-check-input' %>
-      <%= f.label :remember_me, class: 'form-check-label' do %>
-        <%= resource.class.human_attribute_name('remember_me') %>
-      <% end %>
-    </div>
-  <% end %>
+      ## Rememberable
+      t.datetime :remember_created_at
 
-  <div class="form-group">
-    <%= f.submit  ('ログイン'), class: 'btn btn-red50_rsd' ,style:"float:right"%>
-  </div>
-<% end %>
+      ## Trackable
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.string   :current_sign_in_ip
+      # t.string   :last_sign_in_ip
 
-<%= render 'devise/shared/links' %>
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
 
-</div>
+      ## Lockable
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+      t.string :name,　null: false
+      t.text :profile,　null: false
+      t.integer :status, default: 1, null: false, limit: 1
+
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
