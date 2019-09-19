62d6ec03ccabd0e4f005b07d8b1ba556ce2af8c2
rui-watanabe <wataru610806@gmail.com>
2019-09-19 17:34:33 +0900

    refactoring_dm-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,16 +3,16 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, only: [:create]
   
   def create
-    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present? 
       @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id))
       respond_to do |format|
         format.html { redirect_to "/rooms/#{@message.room_id}"}
         format.json
-    end
+      end
     else
       @messages = messages.includes(:user)
-      flash.now[:alert] = "メッセージを入力してください。"
-      render template: "room/new"
+      flash[:alert] = "メッセージを入力してください。"
+      redirect_to "/rooms/#{@message.room_id}"
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   before_action :authenticate_user!, only: [:show]
+  before_action :set_user, only: [:follow, :unfollow, :follow_list, :follower_list, :matching_list, :release, :nonrelease]
 
   def index
     @users = User.where.not(id: current_user.id, status: 0).page(params[:page]).per(5).order("created_at DESC")
@@ -10,8 +11,7 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
-    if @user.id == current_user.id
-    else
+    unless @user.id == current_user.id
       @currentUserEntry.each do |cu|
         @userEntry.each do |u|
           if cu.room_id == u.room_id then
@@ -20,8 +20,7 @@ class UsersController < ApplicationController
           end
         end
       end
-      if @isRoom
-      else
+      unless @isRoom
         @room = Room.new
         @entry = Entry.new
       end
@@ -41,46 +40,43 @@ class UsersController < ApplicationController
   end
 
   def follow
-    @user = User.find(params[:user_id])
     current_user.follow(@user)
     redirect_to user_path(@user)
   end
 
   def unfollow
-    @user = User.find(params[:user_id])
     current_user.stop_following(@user)
     redirect_to user_path(@user)
   end
 
 
   def follow_list
-    @user = User.find(params[:user_id])
   end
 
   def follower_list
-    @user = User.find(params[:user_id])
   end
 
   def matching_list
-    @user = User.find(params[:user_id])
     (current_user.followed_by? @user) && (@user.followed_by? current_user)
   end
 
   def release
-    @user =  User.find(params[:user_id])
     @user.released! unless @user.released?
     flash[:notice]= 'このアカウントを公開しました'
     redirect_to  "/users/#{@user.id}"
   end
 
   def nonrelease
-    @user =  User.find(params[:user_id])
     @user.nonreleased! unless @user.nonreleased?
     flash[:notice]= 'このアカウントを非公開にしました'
     redirect_to "/users/#{@user.id}"
   end
   
   private
+
+  def set_user
+    @user =  User.find(params[:user_id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :avatar, :profile)
