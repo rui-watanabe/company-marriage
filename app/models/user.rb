22857d088797f5adb162c4f6eb7e42131311e315
rui-watanabe <wataru610806@gmail.com>
2019-09-19 20:38:12 +0900

    change_user_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :sex, presence: true
+  validates :avatar, attached: true
+  validates :profile, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email, presence: true
-  validates :name, presence: true
-  validates :profile, presence: true
-  validates :avatar, attached: true
 
   has_one_attached :avatar
 
@@ -16,5 +17,7 @@ class User < ApplicationRecord
   has_many :entries, dependent: :destroy
 
   enum status:{nonreleased: 0, released: 1}
+
+  enum sex: { man: 1, woman: 2 }
       
 end
